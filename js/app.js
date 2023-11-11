6e84c694f97bfdd14d0080d187339e8933a86c04
Rafael Molina Osias <engineer.raflash@gmail.com>
2023-11-11 12:22:57 -0500

    se configuran losprimeros pasos con el Proyecto

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log("proyecto carrito de Compras");
+var carrito = document.getElementById("carrito");
+var contenedorCarrito = document.querySelector("#lista-carrito tbody");
+var vaciarCarritoBtn = document.getElementById("vaciar-carrito");
+var listaCursos = document.getElementById("lista-cursos");
+var cargarEventListeners = function () {
+    if (listaCursos) {
+        listaCursos.addEventListener('click', function (e) {
+            e.preventDefault();
+            if (e.target.classList.contains('agregar-carrito')) {
+                console.log("Agregando al Carrito ...");
+            }
+        });
+    }
+};
+cargarEventListeners();
--- ts/app.ts
+++ ts/app.ts
@@ -1,1 +1,18 @@
-console.log("proyecto carrito de Compras");
+const carrito = document.getElementById("carrito") as HTMLDivElement;
+const contenedorCarrito = document.querySelector("#lista-carrito tbody") as HTMLElement;
+const vaciarCarritoBtn = document.getElementById("vaciar-carrito") as HTMLLinkElement;
+const listaCursos = document.getElementById("lista-cursos") as HTMLDivElement;
+
+const cargarEventListeners :() => void = () => {
+    if(listaCursos){
+        listaCursos.addEventListener('click', (e:MouseEvent) => {
+            e.preventDefault();
+            if ((e.target as HTMLElement).classList.contains('agregar-carrito')) {
+                console.log("Agregando al Carrito ...");
+            }
+        });
+    }
+
+};
+
+cargarEventListeners();
