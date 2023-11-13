3104e89aba93483a8d25f2bff92ce9a021fe89b5
Rafael Molina Osias <engineer.raflash@gmail.com>
2023-11-12 23:37:06 -0500

    se Leen los datos del curso que seleccionamos al dar click

--- js/app.js
+++ js/app.js
@@ -3,12 +3,24 @@ var carrito = document.getElementById("carrito");
 var contenedorCarrito = document.querySelector("#lista-carrito tbody");
 var vaciarCarritoBtn = document.getElementById("vaciar-carrito");
 var listaCursos = document.getElementById("lista-cursos");
+var leerDatosCurso = function (curso) {
+    console.log(curso);
+    var infoCurso = {
+        inmagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    };
+    console.log(infoCurso);
+};
 var cargarEventListeners = function () {
     if (listaCursos) {
         listaCursos.addEventListener('click', function (e) {
             e.preventDefault();
             if (e.target.classList.contains('agregar-carrito')) {
-                console.log("Agregando al Carrito ...");
+                var cursoSeleccionado = e.target.parentElement.parentElement;
+                leerDatosCurso(cursoSeleccionado);
             }
         });
     }
--- ts/app.ts
+++ ts/app.ts
@@ -1,14 +1,36 @@
-const carrito = document.getElementById("carrito") as HTMLDivElement;
-const contenedorCarrito = document.querySelector("#lista-carrito tbody") as HTMLElement;
-const vaciarCarritoBtn = document.getElementById("vaciar-carrito") as HTMLLinkElement;
-const listaCursos = document.getElementById("lista-cursos") as HTMLDivElement;
 
-const cargarEventListeners :() => void = () => {
-    if(listaCursos){
-        listaCursos.addEventListener('click', (e:MouseEvent) => {
+const carrito: HTMLDivElement = document.getElementById("carrito") as HTMLDivElement;
+const contenedorCarrito: HTMLElement = document.querySelector("#lista-carrito tbody") as HTMLElement;
+const vaciarCarritoBtn: HTMLLinkElement = document.getElementById("vaciar-carrito") as HTMLLinkElement;
+const listaCursos: HTMLDivElement = document.getElementById("lista-cursos") as HTMLDivElement;
+
+interface CursoInfo {
+    inmagen: string;
+    titulo: string;
+    precio: string;
+    id: string;
+    cantidad: number;
+}
+
+const leerDatosCurso: (curso: HTMLDivElement) => void = (curso: HTMLDivElement) => {
+    console.log(curso);
+    const infoCurso: CursoInfo = {
+        inmagen: (curso.querySelector('img') as HTMLImageElement).src as string,
+        titulo: (curso.querySelector('h4') as HTMLHeadElement).textContent as string,
+        precio: (curso.querySelector('.precio span') as HTMLSpanElement).textContent as string,
+        id: (curso.querySelector('a') as HTMLAnchorElement).getAttribute('data-id')as string,
+        cantidad: 1
+    };
+    console.log(infoCurso);
+}
+
+const cargarEventListeners: () => void = () => {
+    if (listaCursos) {
+        listaCursos.addEventListener('click', (e: MouseEvent) => {
             e.preventDefault();
             if ((e.target as HTMLElement).classList.contains('agregar-carrito')) {
-                console.log("Agregando al Carrito ...");
+                const cursoSeleccionado: HTMLDivElement = (e.target as HTMLAnchorElement).parentElement!.parentElement as HTMLDivElement;
+                leerDatosCurso(cursoSeleccionado);
             }
         });
     }
